b09f74f5476ce90d40d3c8f9c708c588b697e5ec
Kristen Krofchik <k.krofchik@gmail.com>
2021-02-18 08:16:43 -0600

    completed problems

--- cards.js
+++ cards.js
@@ -1,38 +1,47 @@
-let baseUrl = 'https://deckofcardsapi.com/api/deck'
-let mainGameDeck 
+$(function() {
 
-$.getJSON(`${baseUrl}/new/draw/?count=1`).then(response => {
-    let { suit, value } = response.cards[0];
-    console.log(`${value} of ${suit}`)
-})
+    let baseUrl = 'https://deckofcardsapi.com/api/deck'
+    let mainGameDeck 
 
-let card1
-
-$.getJSON(`${baseUrl}/new/draw/?count=1`)
-    .then(response => {
+    $.getJSON(`${baseUrl}/new/draw/?count=1`).then(response => {
         let { suit, value } = response.cards[0];
-        let deckId = response.deck_id
-        card1 = `${value} of ${suit}`;
-        return $.getJSON(`${baseUrl}/${deckId}/draw/?count=1`);
+        console.log(`${value} of ${suit}`)
     })
-    .then(response2 => {
 
-        let card2Suit = response2.cards[0].suit;
-        let card2Value = response2.cards[0].value;
-        let card2 = `${card2Value} of ${card2Suit}`;
-        console.log(`${card1}, ${card2}`)
+    let card1
+
+    $.getJSON(`${baseUrl}/new/draw/?count=1`)
+        .then(response => {
+            let { suit, value } = response.cards[0];
+            let deckId = response.deck_id
+            card1 = `${value} of ${suit}`;
+            return $.getJSON(`${baseUrl}/${deckId}/draw/?count=1`);
+        })
+        .then(response2 => {
+
+            let card2Suit = response2.cards[0].suit;
+            let card2Value = response2.cards[0].value;
+            let card2 = `${card2Value} of ${card2Suit}`;
+            console.log(`${card1}, ${card2}`)
+        });
+
+    
+    $.getJSON(`${baseUrl}/new/shuffle/`).then(response => {
+        deckId = response.deck_id;
+        $('button').show();
     });
-
-$('button').click(function() {
-    $.getJSON(`${baseUrl}/new/shuffle/?deck_count=1`)
-        .then(response => {
-            mainGameDeck = response.deck_id;
-            return $.getJSON(`${baseUrl}/${mainGameDeck}/draw/?count=1`);
-        })
-        .then(response => {
-            let currentCardImage = response.cards[0].image;
-            $('body').append(`<img src=${currentCardImage}></img>`)
-        })
+    
+    $('button').click(function() {
+        $.getJSON(`${baseUrl}/${deckId}/draw/?count=1`)
+            .then(response => {
+                let currentCardImage = response.cards[0].image;
+                $('body').append(`<img src=${currentCardImage}></img>`);
+                console.log(response.remaining);
+                if(response.remaining === 0) {
+                    $('button').remove();
+                }
+            })
+    })
 })
     
     
