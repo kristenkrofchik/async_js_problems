124af488e70e1ae93893c635bf27661da496cbf0
Kristen Krofchik <k.krofchik@gmail.com>
2021-03-05 22:47:05 -0600

    refactored using async and await

--- cards.js
+++ cards.js
@@ -1,4 +1,4 @@
-$(function() {
+/*$(function() {
 
     let baseUrl = 'https://deckofcardsapi.com/api/deck'
     let mainGameDeck 
@@ -42,6 +42,50 @@ $(function() {
                 }
             })
     })
-})
+})*/
+let baseUrl = 'https://deckofcardsapi.com/api/deck'
+
+async function drawCard() {
+    const res = await axios.get(`${baseUrl}/new/draw/?count=1`);
+    const data = res.data;
+    let { suit, value } = data.cards[0];
+    console.log(`${value} of ${suit}`)
+};
+
+drawCard();
+
+async function draw2Cards() {
+    let res = await axios.get(`${baseUrl}/new/draw/`);
+    let deckId = res.data.deck_id;
+    let res2 = await axios.get(`${baseUrl}/${deckId}/draw/`);
     
+    [res.data ,res2.data].forEach(card => {
+        let { suit, value } = card.cards[0];
+        console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+      });
     
+}
+
+draw2Cards();
+
+
+async function cardGame() {
+    let $btn = $('button');
+
+    let res = await axios.get(`${baseUrl}/new/shuffle/`);
+    $btn.show().on('click', async function() {   
+        let cardData = await axios.get(`${baseUrl}/${res.data.deck_id}/draw/`);
+        let cardSrc = cardData.data.cards[0].image;
+
+        $('body').append(`<img src=${cardSrc}></img>`);
+                if(cardData.data.remaining === 0) {
+                    $('button').remove();
+                    }
+    });
+}
+
+cardGame();
+
+
+
+
--- numbers.js
+++ numbers.js
@@ -1,7 +1,9 @@
 let baseUrl = "http://numbersapi.com"
 let faveNum  = 6
+let myNums = [33, 44, 55]
+let fourNumFacts = []
 
-$.getJSON(`${baseUrl}/${faveNum}/trivia?json`).then(response => {
+/*$.getJSON(`${baseUrl}/${faveNum}/trivia?json`).then(response => {
     trivia = response.text;
     console.log(trivia)
 });
@@ -25,4 +27,37 @@ Promise.all(fourNumFacts)
         for (res of factsArr) {
             $("body").append(`<h3>${res.text}</h3>`);
         }
-    })
+    })*/
+
+async function faveNumFact(num) {
+    const res = await axios.get(`${baseUrl}/${num}/trivia?json`);
+    const trivia = res.data.text;
+    console.log(trivia);
+}
+
+faveNumFact(faveNum);
+
+async function multNumFacts(nums) {
+    const res = await axios.get(`${baseUrl}/${nums}?json`);
+    const facts = Object.values(res.data);
+    for(fact of facts) {
+        $("body").append(`<h3>${fact}</h3>`);
+    }
+}
+
+multNumFacts(myNums);
+
+async function fourFacts(n) {
+    let facts = await Promise.all([
+        $.getJSON(`${baseUrl}/${n}/trivia?json`),
+        $.getJSON(`${baseUrl}/${n}/trivia?json`),
+        $.getJSON(`${baseUrl}/${n}/trivia?json`),
+        $.getJSON(`${baseUrl}/${n}/trivia?json`)
+    ])
+    facts.forEach(data => {
+        $('body').append(`<p>${data.text}</p>`);
+    });
+}
+
+fourFacts(7);
+
